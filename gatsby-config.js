f9f352be6bfd12a2360b00a87bde790396f73235
Alin <alin@neontribe.co.uk>
2020-07-24 20:21:01 +0100

    Add blog and dummy posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,28 @@
 module.exports = {
+    siteMetadata: {
+        title: `Alin Chindea`,
+        author: `Alin 🦕`,
+        description: `Alin's personal site for Gatsby 101 at Codeland`,
+        siteUrl: `https://alin-gatsby-101.netlify.app/`,
+        social: [
+            {
+                name: `Twitter`,
+                url: `https://twitter.com/alinchindea`,
+            },
+            {
+                name: `GitHub`,
+                url: `https://github.com/AlinChindea`
+            }
+        ],
+    },
     plugins: [
-        `gatsby-plugin-mdx`
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-theme-blog`,
+            options: {
+                basePath: `/blog`,
+                mdxOtherwiseConfigured:true,
+            }
+        }
     ]
 }
