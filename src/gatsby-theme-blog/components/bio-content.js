2c3601189338f7204fb4f902c33db039dbf1fe21
Alin <alin@neontribe.co.uk>
2020-07-24 20:38:21 +0100

    Alter bio content, replace avatar

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react"
+
+const BioContent = () => (
+    <Fragment>
+        Words by <a href="https://alinchindea.com/">Alin Chindea</a>.
+        <br />
+    Alin is a web developer 👨🏽‍💻!
+    </Fragment>
+)
+
+export default BioContent
